037f48d4f6bbc70edcb0ba5ecab9a5090d8b7b00
Renato Cesar <re.fbarcellos@hotmail.com>
2021-08-23 07:52:36 -0400

    :construction: Working with circular motion

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
-import pygame
+import pygame as pygame
 import json
 import math
-import random
+from random import randint
+
 
 from Engine.Vector import Vector2D
+from Engine.Glow import Glow
 from Engine import utils
 from Engine.shape import Shape
 from Engine.Particle.shape_particle import ShapeParticle
@@ -23,6 +25,7 @@ pygame.mouse.set_visible(False)
 FONT = pygame.font.Font("res/Pixellari.ttf", 12)
 
 global debugging
+global game_time
 debugging = True
 game_time = 0
 base_screen_size = configs['resolution']
@@ -31,29 +34,20 @@ display = pygame.Surface((300, 200))
 clock = pygame.time.Clock()
 
 
+particleEmitionTime = 0
+angle = 0
+radius = 1
 cursor_img = pygame.transform.scale(pygame.image.load('res/mouse.png').convert(), (33, 33))
-
 cursor_img.set_colorkey((0, 0, 0))
 
-entities = []
-
-
+particles = []
+original_angle = 0
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type==pygame.VIDEORESIZE:
-            w = event.dict['size'][0]
-            h = event.dict['size'][1]
-            screen=pygame.display.set_mode(event.dict['size'],pygame.RESIZABLE)
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_F1 :
-                debugging = not debugging
-            if event.key == pygame.K_SPACE:
-                pass
-    
 
     mx, my = pygame.mouse.get_pos()
     true_mx = mx
@@ -63,11 +57,37 @@ while running:
     mx /= base_screen_size[0] / display.get_width()
     my /= base_screen_size[1] / display.get_height()
 
-    display.fill((0,0,40))
+    display.fill((0,40,40))
+    position = 120    
+    pygame.draw.rect(display, (255,5,5), 
+                    pygame.Rect(position, position, 2,2), 
+                    width = 0,border_radius = 100)
+    
+    
+    particleEmitionTime -=1
+    if particleEmitionTime < 0:
+        particles.append(ShapeParticle(Vector2D(position,position), Vector2D(2,2), life_time = 100, color = (255,255,255),opacity = 215,width= 2, height = 2))
+        particleEmitionTime = 50
 
-    if debugging:
-        utils.draw_text(FONT, "FPS: " + str(int(clock.get_fps())), display, (10,10))
-  
+    
+    angle += 0.0002
+
+    for i, particle in sorted(enumerate(particles), reverse=True):
+            particle.life_time -= 0.1
+            particle.position.x += math.sin(angle * 120)
+            particle.position.y += math.cos(angle * 120)
+
+            # particle.rotation += randint(1,10)
+            particle.Draw(display)
+
+            if particle.life_time <= 0:
+                particles.pop(i)   
+
+
+
+
+
+
 
     screen.blit(pygame.transform.scale(display, base_screen_size),
                 ((screen.get_width() - base_screen_size[0]) // 2,
